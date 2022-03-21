cf4241664bddc4a86223835a956d1767641d438d
Uduak Essien <acushla4real@gmail.com>
2022-03-21 13:33:11 +0100

    Add resolvers and graphql schema

--- server/server.js
+++ server/server.js
@@ -1,9 +1,15 @@
+const { ApolloServer, gql} = require('apollo-server-express');
+const fs = require('fs');
+const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const express = require('express');
 const expressJwt = require('express-jwt');
 const jwt = require('jsonwebtoken');
 const db = require('./db');
+
+const filePath = './schema.graphql';
+const pathToFile = path.join(__dirname, filePath);
 
 const port = 9000;
 const jwtSecret = Buffer.from('Zn8Q5tyZ/G1MHltc4F/gTkVJMlrbKiZt', 'base64');
@@ -13,6 +19,11 @@ app.use(cors(), bodyParser.json(), expressJwt({
   secret: jwtSecret,
   credentialsRequired: false
 }));
+
+const typeDefs = gql(fs.readFileSync('./schema.graphql', {encoding: 'utf-8'}));
+const resolvers = require('./resolvers');
+const apolloServer = new ApolloServer({typeDefs, resolvers});
+apolloServer.applyMiddleware({app, path: '/graphql'});
 
 app.post('/login', (req, res) => {
   const {email, password} = req.body;
