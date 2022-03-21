d91360e19e192304a5e1497e3dcf76ca0514810a
Uduak Essien <acushla4real@gmail.com>
2022-03-21 15:14:10 +0100

    Add query for company

--- server/resolvers.js
+++ server/resolvers.js
@@ -3,6 +3,8 @@ const db = require('./db');
 const Query = {
   job: (root, {id}) => db.jobs.get(id),
   jobs: () => db.jobs.list(),
+  company: (root, {id}) => db.companies.get(id),
+  companies: () => db.companies.list(),
 }
 
 const Job = {
